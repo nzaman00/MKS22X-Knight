55796e73b5564393dd84a3c81a362b6665445211
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-02-13 10:09:08 -0500

    Update KnightBoard.java

--- KnightBoard.java
+++ KnightBoard.java
@@ -12,5 +12,16 @@ public class KnightBoard{
       }
     }
   }
+  public String toString(){
+    String s = "";
+  	for (int i = 0; i < board.length; i++) {
+  		for (int j = 0; j < board[0].length; j++) {
+  			if (board[i][j] < 10) s = s + " " + board[i][j] + " ";
+  			else s += board[i][j] + " ";
+  		}
+  		s+="\n";
+  	 }
+    return s;
+    }
 
 }
