b029ca8d5adcbec72078f47b024a4b51ca70e5be
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-02-13 10:08:36 -0500

    Update KnightBoard.java

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,3 +1,16 @@
 public class KnightBoard{
+  private int[][] board;
+  private int[][] moves;
+  public KnightBoard(int startingRows,int startingCols){
+    if( startingCols <= 0 || startingRows <= 0){
+      throw new IllegalArgumentException();
+    }
+    board = new int[startingRows][startingCols];
+    for(int i =0; i < board.length;i++ ){
+      for(int j = 0; j < board[0].length;j++){
+        board[i][j]= 0;
+      }
+    }
+  }
 
 }
