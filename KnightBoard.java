c804a2cef292cce8bd47c1dc5470405b56c4356c
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-02-24 12:50:16 -0500

    fixed toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,27 +1,37 @@
 public class KnightBoard{
   private int[][] board;
-  private int[][] moves;
+  private int[] movesR = {-2, -1, 1, 2, 2, 1, -1, -2};
+  private int[] movesC = {1, 2, 2, 1, -1, -2, -2, -1};
+  
   public KnightBoard(int startingRows,int startingCols){
     if( startingCols <= 0 || startingRows <= 0){
       throw new IllegalArgumentException();
     }
     board = new int[startingRows][startingCols];
-    for(int i =0; i < board.length;i++ ){
-      for(int j = 0; j < board[0].length;j++){
-        board[i][j]= 0;
-      }
-    }
   }
+  
   public String toString(){
-    String s = "";
-  	for (int i = 0; i < board.length; i++) {
-  		for (int j = 0; j < board[0].length; j++) {
-  			if (board[i][j] < 10) s = s + " " + board[i][j] + " ";
-  			else s += board[i][j] + " ";
-  		}
-  		s+="\n";
-  	 }
-    return s;
+    String ans = "";
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == 0) {
+          if (board.length * board[0].length >= 10) {
+            ans += " _ ";
+          } else {
+            ans += "_ ";
+          }
+        } else {
+          if (board.length * board[0].length >= 10 && board[i][j] / 10 == 0) {
+            ans = ans + " " + board[i][j] + " ";
+          } else {
+            ans = ans + board[i][j] + " ";
+          }
+        }
+      }
+      ans += '\n';
     }
-
+    return ans;
+  }
+  
+  
 }
